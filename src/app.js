c0d4e73ceb3056ac57ec2327b0d906c0fe2aa89c
Paul <rpmstevenson@gmail.com>
2018-06-25 16:32:03 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// const InputView = require('./views/input_view.js');
+// const WordCounter = require('./models/word_counter.js');
+// const ResultView = require('./views/result_view.js');
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("JS loaded");
+})
---
+++ src/views/input_view.js
@@ -0,0 +1,12 @@
+const PubSub = require( '..helpers/pub_sub.js')
+
+const InputView = function () {
+
+};
+
+InputView.prototype.bindEvents = function () {
+    const input = document.querySelector('#wordcounter-form')
+
+};
+
+module.exports = InputView;
